779331726c7d728b537114e0520b2be6b1d28b56
sumit vaise <sumitvaise29@gmail.com>
2019-02-12 13:16:48 -0500

    Added IM code

--- MIPS.vhd
+++ MIPS.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+USE IEEE.NUMERIC_STD.ALL;
 
 entity MIPS is
   port (
@@ -10,10 +11,21 @@ end MIPS ;
 
 architecture Behavior of MIPS is
 
-  signal
+  -- setting the default address as 0
+  -- input signal from PC to instruction memory to read the instruction from the address provided by PC
+  signal CurrentInstructionAddress : std_logic_vector(31 downto 0) := (others => 0);
+
+  -- ouput signal from instruction memory [ 32 bit ]
+  signal Instruction : std_logic_vector(31 downto 0) := (others => 0); 
 
 begin
-
+  process(clk,reset)
+  begin
+      U1 : ENTITY work.InstructionMemory(Behavioral)
+      PORT MAP(
+        InstructionAddress => CurrentInstructionAddress,
+        Instruction => Instruction
+  );
 
 
 end architecture ; -- Behavior

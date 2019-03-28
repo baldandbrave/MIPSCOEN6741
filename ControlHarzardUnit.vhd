b7fe95b8279607722f0e7358ac3588771f3a4f24
baldandbrave <onthewayjing96@gmail.com>
2019-03-28 15:05:17 -0400

    Fix error, finish CHU simulation

--- ControlHarzardUnit.vhd
+++ ControlHarzardUnit.vhd
@@ -5,6 +5,8 @@
 library ieee ;
     use ieee.std_logic_1164.all ;
     use ieee.numeric_std.all ;
+    use ieee.std_logic_unsigned.all;
+
 
 entity ControlHarzardUnit is
   port (
@@ -15,7 +17,7 @@ entity ControlHarzardUnit is
     Immediate : in std_logic_vector(31 downto 0) ;
     OpCode    : in std_logic_vector(2 downto 0) ; -- 3bits opcode
     Funct     : in std_logic_vector(8 downto 0) ; -- 9bits funct
-    NewPC     : out std_logic_vector(31 downto 0;
+    NewPC     : out std_logic_vector(31 downto 0);
 
     -- Flush IF/ID to all 0, insert bubble, stall pipeline.
     -- all 0: and r0, r0, r0, doesn't change value of $zero.
@@ -25,16 +27,18 @@ end ControlHarzardUnit ;
 
 architecture Behavior of ControlHarzardUnit is
   signal FlushCounter : integer := 0;
+  signal XorResult    : std_logic_vector(31 downto 0) ;
 begin
     
   ControlHarzardUnit : process(OpCode, Funct, ReadData1, ReadData2, Clock)
     begin
-      if (ReadData1 xor ReadData2 = x"00000000") and (Opcode = "011") then -- beq
-        NewPC <= PCPlus4 + Immediate;
-        FlushCounter = 1;
+      XorResult <= ReadData1 xor ReadData2;
+      if (XorResult= x"00000000") and (Opcode = "011") then -- beq
+        NewPC <= PCPlus4 + Immediate ; 
+        FlushCounter <= 1;
       elsif OpCode = "000" and Funct = "000001000" then -- jr
-        NewPC <= ReadData1/2; -- TODO: which one?
-        FlushCounter = 1;
+        NewPC <= ReadData1; -- TODO: which one?
+        FlushCounter <= 1;
       else
         NewPC <= PCPlus4; -- no branch
       end if ;

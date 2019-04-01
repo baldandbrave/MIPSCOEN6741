e1b8c1cf3746e297e9718571cd608cf5d819aa86
baldandbrave <onthewayjing96@gmail.com>
2019-04-01 15:25:28 -0400

    finish TODOs, need to change the component format in MIPS.vhd

--- EX_MEM.vhd
+++ EX_MEM.vhd
@@ -9,22 +9,22 @@ entity EX_MEM is
 
     ALUResultIn     : in std_logic_vector(31 downto 0) ;
     ReadData2In     : in std_logic_vector(31 downto 0) ;
-    TargetRegIn        : in std_logic_vector(4 downto 0) ; -- from mux of rt&rd
+    TargetRegIn     : in std_logic_vector(4 downto 0) ; -- from mux of rt&rd
 
     -- MEM signals
     MemRead         : in std_logic;
     MemWrite        : in std_logic;
     -- WB signals
     MemToReg        : in std_logic;
-    RegWrite          : in std_logic;
+    RegWrite        : in std_logic;
 
     ALUResultOut    : out std_logic_vector(31 downto 0) ;
     ReadData2Out    : out std_logic_vector(31 downto 0) ; -- to DM WriteData
-    TargetRegOut       : out std_logic_vector(4 downto 0) ; -- to Reg
+    TargetRegOut    : out std_logic_vector(4 downto 0) ; -- to Reg
     MemReadOut      : out std_logic;
     MemWriteOut     : out std_logic;
     MemToRegOut     : out std_logic;
-    RegWriteOut       : out std_logic
+    RegWriteOut     : out std_logic
   ) ;
 end EX_MEM ; 
 
